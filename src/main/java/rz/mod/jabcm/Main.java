de6566c9142dc4c50cc6f2cc4b4f8ef9e35c3104
RavenholmZombie <supersoftware1234@gmail.com>
2021-02-16 22:52:38 -0500

    Incremented version to match 1.12.2 release

--- src/main/java/rz/mod/jabcm/Main.java
+++ src/main/java/rz/mod/jabcm/Main.java
@@ -18,7 +18,7 @@ public class Main
 {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "jabcm";
-    public static final String VERSION = "1.16.3-0.0.6";
+    public static final String VERSION = "1.16.3-0.0.6_A";
 
     public Main()
     {
